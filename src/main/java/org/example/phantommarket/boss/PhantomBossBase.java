5011ab1f4a932e04d905e426b2ffc42a742abc3d
TheHikimori <darksilver175@gmail.com>
2026-03-20 19:59:58 +0200

    Delete src/main directory

--- src/main/java/org/example/phantommarket/boss/PhantomBossBase.java
+++
@@ -1,9 +0,0 @@
-package org.example.phantommarket.boss;
-
-import org.bukkit.Location;
-import org.bukkit.entity.LivingEntity;
-
-public interface PhantomBossBase {
-    LivingEntity spawnLegendary(Location loc);
-    LivingEntity spawnNormal(Location loc);
-}
